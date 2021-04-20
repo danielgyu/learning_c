2d136548d6b8fd2308aabc6e9576b5db49668a00
kungyu <hismayfly@gmail.com>
2021-04-20 09:46:41 +0900

    byte_ordering

---
+++ struct_6/byte_ordering.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+#define NAME_LEN (32)
+
+int main(void)
+{
+	typedef struct name {
+		char firstname[NAME_LEN];
+		char lastname[NAME_LEN];
+	} name_t;
+	
+	typedef struct user_info {
+		unsigned int id;
+		name_t name;
+		unsigned short height;
+		float weight;
+		unsigned short age;
+	} user_info_t;
+
+	user_info_t user;
+
+	char firstname[] = "LuLu";
+	user.id = 1234;
+
+	strncpy(user.name.firstname, firstname, NAME_LEN);
+
+	printf("%d\n", user.id);
+	printf("%s\n", user.name.firstname);
+}
