aa58cb4ee1122f5a946ebef38708045bb8bf7e19
kungyu <hismayfly@gmail.com>
2021-04-27 10:05:13 +0900

    printf variadic function

---
+++ struct_6/printf.c
@@ -0,0 +1,49 @@
+static void print_int_recursive(unsigned dint val)
+{
+	if (val == 0) {
+		return;
+	}
+
+	print_int_recursive(val / 10);
+	putchar('0' + val % 10);
+}
+
+
+void printf_simple(const char* format, ...)
+{
+	va_list ap;
+
+	va_start(ap, format);
+	{
+		while (*format != '\0') {
+			unsigned val;
+			const char* str;
+
+			switch (*format) {
+				case 's':
+					str = va_arg(ap, const char*);
+					while (*str != '\0') {
+						putchar(*str++);
+					}
+					break;
+				case 'c':
+					val = va_arg(ap, unsigned int);
+					putchar(val);
+					break;
+				case 'd':
+					val = va_arg(ap, unsigned int);
+					print_int_recursive(val);
+					break;
+				default:
+					putchar(*format);
+					break;
+			}
+		}
+	}
+}
+
+
+int main(void)
+{
+	printf_simple()
+}
