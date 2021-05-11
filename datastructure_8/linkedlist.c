892a47d9a48a8670e0b78c08ac25f0d1a20054ca
kungyu <hismayfly@gmail.com>
2021-05-12 00:46:31 +0900

    sorted linked list

--- datastructure_8/linkedlist.c
+++ datastructure_8/linkedlist.c
@@ -15,3 +15,37 @@ void print_node(const node_t* head)
 		p = p->next;
 	}
 }
+
+void destroy(node_t* head)
+{
+	node_t* p = head;
+
+	while (p != NULL) {
+		node_t* next = p->next;
+		free(p);
+		p = next;
+	}
+}
+
+void insert_front(node_t** phead, int n)
+{
+	node_t* new_node;
+
+	new_node = malloc(sizeof(node_t));
+	new_node->value = n;
+	new_node->next = *phead;	
+	*phead = new_node;
+}
+
+int main(void)
+{
+	node_t* head = NULL;
+
+	insert_front(&head, 3);
+	insert_front(&head, 5);
+	insert_front(&head, 2);
+	insert_front(&head, 0);
+
+	destroy(head);
+	head = NULL;
+}
---
+++ datastructure_8/sorted.c
@@ -0,0 +1,30 @@
+void insert_sorted(node_t** phead, int n)
+{
+	node_t** pp;
+	node_t* new_node;
+
+	new_node = malloc(sizeof(node_t));
+	new_node->value = n;
+
+	pp = phead;
+	while(*pp != NULL) {
+		if((*pp)->value >= n) {
+			break;
+		}
+		pp = &(*pp)->next;
+	}
+	new_node->next = *pp;
+	*pp = new_node;
+}
+
+int main(void)
+{
+	node_t* head = NULL;
+
+	insert_sorted(&head, 3);
+	insert_sorted(&head, 5);
+	insert_sorted(&head, 2);
+	insert_sorted(&head, 0);
+
+	head = NULL;
+}
